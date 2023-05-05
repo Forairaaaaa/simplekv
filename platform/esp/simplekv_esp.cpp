e232c085b6cbe287fea57a375ef8387207d5c378
Forairaaaaa <applesyqd@outlook.com>
2023-05-05 12:27:39 +0800

    adding esp platform

---
+++ platform/esp/simplekv_esp.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file simplekv_esp.cpp
+ * @author Forairaaaaa
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "simplekv_esp.h"
+
---
+++ platform/esp/simplekv_esp.h
@@ -0,0 +1,16 @@
+/**
+ * @file simplekv_esp.h
+ * @author Forairaaaaa
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#pragma once
+#include "../../simplekv.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+
