7bf95b0f3aa4097383e7db14c5f461c6124c3210
Forairaaaaa <applesyqd@outlook.com>
2023-08-15 09:43:07 +0800

    add example

--- cmake/basic.cmake
+++ cmake/basic.cmake
@@ -1,11 +1,21 @@
 # Basic CMakeLists.txt
 
 set(SIMPLEKV_SRCS
-    "simplekv.cpp"
+    ./simplekv.cpp
 )
-
-add_library(simplekv ${SIMPLEKV_SRCS})
-
-target_include_directories(simplekv PUBLIC
-    "."
+set(SIMPLEKV_INCS
+    ./
 )
+add_library(${PROJECT_NAME} ${SIMPLEKV_SRCS})
+target_include_directories(${PROJECT_NAME} PUBLIC ${SIMPLEKV_INCS})
+
+
+option(SIMPLEKV_BUILD_EXAMPLE "Build example" ON)
+
+
+# Example
+if(SIMPLEKV_BUILD_EXAMPLE)
+    add_subdirectory(./example/)
+endif()
+
+
