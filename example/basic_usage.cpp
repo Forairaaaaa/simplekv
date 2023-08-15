f38624db52ed8be17efecc518f66a250137021cc
Forairaaaaa <applesyqd@outlook.com>
2023-08-15 11:59:33 +0800

    change to unordered map

--- example/basic_usage.cpp
+++ example/basic_usage.cpp
@@ -16,8 +16,9 @@ int main(int argc, char const *argv[])
     SIMPLEKV::SimpleKV db;
 
 
-    /* ------------------------------------- Basic types ------------------------------------- */
-    printf("\n------------------------------------- Basic types -------------------------------------\n");
+
+    /* -------------------------------------------------------------- */
+    printf("\n[Basic types]\n");
 
     db.Add<int>("Age", 666);
     printf("Age: %d\n", db.Get("Age")->value<int>());
@@ -34,27 +35,30 @@ int main(int argc, char const *argv[])
     // > 6.666000
     printf("%.10f\n", db.Get("double")->value<double>());
     // > 2333.3333333333
-    /* ------------------------------------- Basic types ------------------------------------- */
+    /* -------------------------------------------------------------- */
 
 
 
-    /* ------------------------------------- Array ------------------------------------- */
-    printf("\n------------------------------------- Array -------------------------------------\n");
+    /* -------------------------------------------------------------- */
+    printf("\n[Array]\n");
+
     uint16_t data[10] = {0, 3, 6, 9, 12, 15, 18, 21, 24, 27};
 
     db.Add("Data", data);
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         printf("%d ", db.Get("Data")->value<uint16_t*>()[i]);
     }
     printf("\n");
     // > 0 3 6 9 12 15 18 21 24 27
-    /* ------------------------------------- Array ------------------------------------- */
-
+    /* -------------------------------------------------------------- */
     
 
-    /* ------------------------------------- Custom type ------------------------------------- */
-    printf("\n------------------------------------- Custom type -------------------------------------\n");
+
+    /* -------------------------------------------------------------- */
+    printf("\n[Custom type]\n");
+
     struct MyType_t
     {
         int a;
@@ -76,14 +80,99 @@ int main(int argc, char const *argv[])
     my_type_temp = db.Get("MyType_2")->value<MyType_t>();
     printf("%d %.2f %s\n", my_type_temp.a, my_type_temp.b, my_type_temp.status.c_str());
     // > -22 -2.22 wohao
-    /* ------------------------------------- Custom type ------------------------------------- */
+    /* -------------------------------------------------------------- */
 
 
 
+    /* -------------------------------------------------------------- */
+    printf("\n[Database info api]\n");
+
+    printf("database has %ld elements\n", db.Size());
+    printf("database's memory usage: %ld\n\n", db.MemoryUsage());
+
+
+
+    std::string key = "MyType_1";
+    printf("delete data \"%s\"\n", key.c_str());
+    db.Delete(key);
+
+    printf("database has %ld elements\n", db.Size());
+    printf("database's memory usage: %ld\n\n", db.MemoryUsage());
+
+
+
+    printf("delete all data\n");
+    db.DeleteAll();
+
+    printf("database has %ld elements\n", db.Size());
+    printf("database's memory usage: %ld\n\n", db.MemoryUsage());
+
+
+
+    key = ":)";
+    printf("is data \"%s\" exist? %s\n", key.c_str(), db.Exist(key) ? "yes" : "no");
+
+    printf("add data \"%s\"\n", key.c_str());
+    db.Add(key, std::string("qwq qwq qwq qwq"));
+
+    printf("is data \"%s\" exist? %s\n", key.c_str(), db.Exist(key) ? "yes" : "no");
+
+    printf("database has %ld elements\n", db.Size());
+    printf("database's memory usage: %ld\n", db.MemoryUsage());
+    /* -------------------------------------------------------------- */
+
+
+
+    /* -------------------------------------------------------------- */
+    printf("\n[Notice]\n");
+    
+    /* Because the database is actually storing the pointers of data buffer */
+    /* When try to store a container like this */
+    db.Add<std::string>("cppstring", "w.w w.w w.w w.w");
+    printf("%s\n", db.Get("cppstring")->value<std::string>().c_str());
+
+    /* It's normal when the address stay still */
+    db.Put<std::string>("cppstring", ":( :(");
+    printf("%s\n", db.Get("cppstring")->value<std::string>().c_str());
+
+    /* But when the address moves */
+    /* It goes wrong */
+    db.Put<std::string>("cppstring", ":( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( :( ");
+    printf("%s\n", db.Get("cppstring")->value<std::string>().c_str());
+    /* Same as str::vector */
+
+
+    db.Delete("cppstring");
+    static std::string haha = ":)";
+
+
+    /* So the best way is to store the container's pointer */
+    db.Add<std::string*>("cppstring", &haha);
+    printf("%s\n", db.Get("cppstring")->value<std::string*>()->c_str());
+
+    /* Because database only storing the address of the container */
+    /* Wherever it goes */
+    haha = "so coooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooool!";
+    printf("%s\n", db.Get("cppstring")->value<std::string*>()->c_str());
+
+    /* System will get it for you */
+    haha = "~";
+    printf("%s\n", db.Get("cppstring")->value<std::string*>()->c_str());
+    /* -------------------------------------------------------------- */
+
+
+
+    /* -------------------------------------------------------------- */
     /* << Bug >>: Add like this causes string become the same, don't know why */
-    printf("\n<< Bug >>: Add like this causes string become the same, don't know why\n");
-    db.Add("MyType_3", my_type_1);
-    db.Add("MyType_4", my_type_2);
+    printf("\n[BUG]\n");
+    printf("<< Bug >>: Add like this causes string become the same, don't know why\n");
+
+    // db.Add("MyType_3", my_type_1);
+    // db.Add("MyType_4", my_type_2);
+
+    /* This ok */
+    db.Add("MyType_3", (void*)&my_type_1, sizeof(MyType_t));
+    db.Add("MyType_4", (void*)&my_type_2, sizeof(MyType_t));
 
     my_type_temp = db.Get("MyType_3")->value<MyType_t>();
     printf("%d %.2f %s\n", my_type_temp.a, my_type_temp.b, my_type_temp.status.c_str());
@@ -92,7 +181,7 @@ int main(int argc, char const *argv[])
     my_type_temp = db.Get("MyType_4")->value<MyType_t>();
     printf("%d %.2f %s\n", my_type_temp.a, my_type_temp.b, my_type_temp.status.c_str());
     // > -22 -2.22 wohao
-
+    /* -------------------------------------------------------------- */
 
 
     return 0;
